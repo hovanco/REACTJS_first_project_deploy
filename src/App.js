092dec8ea0a66d52058e41fd06db8a1599151c64
hovanco <hovancoquangtri@gmail.com>
2020-09-07 22:19:46 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="main_container">
           <div className="item">
             <div className="img_holder">
-              <img src={pic_1} alt="image nice" />
+              <img src={pic_1} aria-hidden alt="image nice" />
             </div>
             <div className="item_info">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum est, voluptas cupiditate assumenda iure vero tempora fugiat. Unde blanditiis incidunt alias molestiae. Totam, laboriosam. Animi accusamus et labore sit nihil exercitationem provident odit nostrum ipsum, architecto deserunt eius veritatis qui enim modi impedit nemo quam quae voluptatibus aliquid possimus.
@@ -22,7 +22,7 @@ class App extends Component {
           </div>  
           <div className="item">
             <div className="img_holder">
-            <img src={pic_2} alt="nice" />
+            <img src={pic_2} aria-hidden alt="nice" />
             </div>
             <div className="item_info">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum est, voluptas cupiditate assumenda iure vero tempora fugiat. Unde blanditiis incidunt alias molestiae. Totam, laboriosam. Animi accusamus et labore sit nihil exercitationem provident odit nostrum ipsum, architecto deserunt eius veritatis qui enim modi impedit nemo quam quae voluptatibus aliquid possimus.
@@ -30,7 +30,7 @@ class App extends Component {
           </div>  
           <div className="item">
             <div className="img_holder">
-            <img src={pic_3} alt="nice" />
+            <img src={pic_3} aria-hidden alt="nice" />
             </div>
             <div className="item_info">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum est, voluptas cupiditate assumenda iure vero tempora fugiat. Unde blanditiis incidunt alias molestiae. Totam, laboriosam. Animi accusamus et labore sit nihil exercitationem provident odit nostrum ipsum, architecto deserunt eius veritatis qui enim modi impedit nemo quam quae voluptatibus aliquid possimus.
@@ -38,7 +38,7 @@ class App extends Component {
           </div>  
           <div className="item">
             <div className="img_holder">
-            <img src={pic_4} alt="nice" />
+            <img src={pic_4} aria-hidden alt="nice" />
             </div>
             <div className="item_info">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum est, voluptas cupiditate assumenda iure vero tempora fugiat. Unde blanditiis incidunt alias molestiae. Totam, laboriosam. Animi accusamus et labore sit nihil exercitationem provident odit nostrum ipsum, architecto deserunt eius veritatis qui enim modi impedit nemo quam quae voluptatibus aliquid possimus.
@@ -46,7 +46,7 @@ class App extends Component {
           </div>  
           <div className="item">
             <div className="img_holder">
-            <img src={pic_5} alt="nice" />
+            <img src={pic_5} aria-hidden alt="nice" />
             </div>
             <div className="item_info">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rerum est, voluptas cupiditate assumenda iure vero tempora fugiat. Unde blanditiis incidunt alias molestiae. Totam, laboriosam. Animi accusamus et labore sit nihil exercitationem provident odit nostrum ipsum, architecto deserunt eius veritatis qui enim modi impedit nemo quam quae voluptatibus aliquid possimus.
